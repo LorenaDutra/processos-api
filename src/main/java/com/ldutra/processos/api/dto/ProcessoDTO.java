6ec1dd360f2cc186db69556182b23992f703a526
Lorena Dutra <ldutra.info@gmail.com>
2020-04-09 16:10:18 -0300

    Concluindo o controller de processos

---
+++ src/main/java/com/ldutra/processos/api/dto/ProcessoDTO.java
@@ -0,0 +1,22 @@
+package com.ldutra.processos.api.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcessoDTO {
+
+	private Long id;
+	private String parecer;
+	private Integer mes;
+	private Integer ano;
+	private Long usuario;
+	private String status;
+}
